c0fbe2509032df7d94809aefc3fa426700cda84c
Gabriel Neander <114587036+gabriel-neander@users.noreply.github.com~git config --global user.email 114587036+gabriel-neander@users.noreply.github.com~>
2023-08-27 19:24:53 -0300

    Sound effect added.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@
 function bling() {
     let icon = document.querySelector("#bling");
     icon.style.display = "block";
+    let clickSound = document.querySelector("#clickSound");
+    clickSound.play();
         setTimeout(function() {
         icon.style.display = "none";
-    }, 300);
+    }, 200);
 }
 
