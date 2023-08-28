f66f8b439f0ce3b2ab42cbbb3eaf7e46e2cd975e
Gabriel Neander <114587036+gabriel-neander@users.noreply.github.com~git config --global user.email 114587036+gabriel-neander@users.noreply.github.com~>
2023-08-27 23:38:32 -0300

    Basic functions made.

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,56 @@ function bling() {
     clickSound.play();
         setTimeout(function() {
         icon.style.display = "none";
-    }, 200);
+    }, 300);
 }
 
+let display = document.getElementById("display");
+let formula = "";
+
+// Add click event listener to buttons
+let buttons = document.querySelectorAll(".button");
+buttons.forEach(button => {
+    button.addEventListener("click", function() {
+        let buttonName = this.getAttribute("name");
+
+        if (buttonName === "=") {
+            calculate();
+        } else {
+            formula += buttonName;
+            display.textContent = formula;
+        }
+    });
+});
+
+function calculate() {
+    // Split the formula into numbers and operators
+    let numbers = formula.split(/[\+\-\x\÷\^]/);
+    let operators = formula.match(/[\+\-\x\÷\^]/g);
+
+    // Initialize the result with the first number
+    let result = parseInt(numbers[0]);
+
+    // Perform calculations based on operators and numbers
+    for (let i = 0; i < operators.length; i++) {
+        let operator = operators[i];
+        let number = parseInt(numbers[i + 1]);
+    
+        if (operator === "+") {
+            result += number;
+        } else if (operator === "-") {
+            result -= number;
+        } else if (operator === "x") {
+            result *= number;
+        } else if (operator === "÷") {
+            result /= number;
+        } else if (operator === "^") {
+            result **= number;
+        } else { result = ""}
+    }
+    display.textContent = result;
+    formula = "";
+}
+function resetFormulaAndDisplay() {
+    formula = "";
+    document.getElementById("display").textContent = "0";
+}
